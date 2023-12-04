5d4b2f089abff9efb5703cf7f8efa21cf36b482d
minhyeokss <minhyeok9303@naver.com>
2023-12-04 18:18:13 +0900

    리뷰 추가

--- src/main/java/com/dogether/controller/PlaceController.java
+++ src/main/java/com/dogether/controller/PlaceController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dogether.domain.Place;
+import com.dogether.domain.Review;
 import com.dogether.service.PlaceService;
+import com.dogether.service.ReviewService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,31 +18,41 @@ import lombok.RequiredArgsConstructor;
 public class PlaceController {
 
     private final PlaceService placeService;
+    private final ReviewService reviewService;
 
     // 완성
     @GetMapping("/place_list")
-    public String getPlaceList(String place_category, Model model) {
+    public List<Place> getPlaceList(String place_category, Model model) {
         List<Place> places = placeService.list(place_category);
-        model.addAttribute("places", places);
+        // model.addAttribute("places", places);
 
         // 테스트용 조건문
         if (!places.isEmpty()) {
             // 인덱스 0의 place_name 출력. 정상
             System.out.println(places.get(0).getPlace_name());
         } else {
-            System.out.println("places is null");
+            System.out.println("place is null");
         }
-        return "place_list";
+        return places;
     }
 
     // 완성
     @GetMapping("/place_detail")
-    public String getPlaceDetail(int place_id, Model model) {
+    public Place getPlaceDetail(int place_id, Model model) {
         Place place = placeService.detail(place_id);
+        List<Review> reviews = reviewService.list(place_id);
+        model.addAttribute("reviews", reviews);
         model.addAttribute("place", place);
-        
+
         // 테스트용 출력 : place id : place_name 출력. 정상
-        System.out.println(place.getPlace_id() + " : " +  place.getPlace_name());
-        return "place_detail";
+        System.out.println(place.getPlace_id() + " : " + place.getPlace_name());
+        if (!reviews.isEmpty()) {
+            for (Review review : reviews) {
+                System.out.println(review.getReview_title());
+            }
+        } else {
+            System.out.println("review is null");
+        }
+        return place;
     }
 }
