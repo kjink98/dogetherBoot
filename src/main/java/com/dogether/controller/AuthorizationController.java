fe65bf1760a550747667f614707ea4a7038fd42d
김진광 <113023365+kjink98@users.noreply.github.com>
2023-12-11 15:49:54 +0900

    ViewController와 RestController 분리

--- src/main/java/com/dogether/controller/AuthorizationController.java
+++ src/main/java/com/dogether/controller/AuthorizationController.java
@@ -1,52 +1,41 @@
 package com.dogether.controller;
 
-import org.apache.ibatis.javassist.bytecode.DuplicateMemberException;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.dogether.domain.User;
 import com.dogether.service.RegisterUserService;
 
-/**
- * 사용자 인증(회원가입)과 관련된 요청을 처리하는 컨트롤러
- */
-@Controller
+@RestController
 @RequestMapping("/user")
-// 회원가입 로직
 public class AuthorizationController {
-	private final RegisterUserService registerUserService;
+    private final RegisterUserService registerUserService;
 
-	public AuthorizationController(RegisterUserService registerUserService) {
-		this.registerUserService = registerUserService;
-	}
-	/**
-	 * 사용자가 회원가입 요청을 하면 이 메소드가 호출
-	 * 사용자가 입력한 정보를 User 객체로 받아와서 회원가입 서비스를 호출
-	 * 회원가입 성공 시 회원가입 성공 페이지로 이동하고, 실패 시 에러 메시지와 함께 회원가입 페이지로 리다이렉트
-	 */
-	@PostMapping("/signup")
-	public String signup(@ModelAttribute User user, Model model, RedirectAttributes rttr) throws DuplicateMemberException {
-		try {
-	        registerUserService.signup(
-	            user.getUser_id(),
-	            user.getUser_pw(),
-	            user.getUser_email(),
-	            user.getUser_name(),
-	            user.getUser_nickname(),
-	            user.getUser_gender(),
-	            user.getUser_regdate(),
-	            user.getUser_birthday(), 
-	            user.getRole());
-	        model.addAttribute("user", user);
-	        return "user/signupSuccess";
-	    } catch (Exception e) {
-	        rttr.addFlashAttribute("msg", "회원 가입 중 오류가 발생했습니다. 다시 시도해 주세요.");
-	        System.out.println(e);
-	        return "redirect:/user/signup";
-	    }
-}
+    public AuthorizationController(RegisterUserService registerUserService) {
+        this.registerUserService = registerUserService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@ModelAttribute User user) {
+        try {
+            registerUserService.signup(
+                user.getUser_id(),
+                user.getUser_pw(),
+                user.getUser_email(),
+                user.getUser_name(),	
+                user.getUser_nickname(),
+                user.getUser_gender(),
+                user.getUser_regdate(),
+                user.getUser_birthday(), 
+                user.getRole()
+            );
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
 }
--- src/main/java/com/dogether/controller/UserViewController.java
+++ src/main/java/com/dogether/controller/UserViewController.java
@@ -1,29 +1,21 @@
 package com.dogether.controller;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.dogether.domain.User;
-import com.dogether.dto.ChangePasswordRequestDto;
 import com.dogether.service.UserService;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 /**
  * 사용자 관련된 요청을 처리하는 컨트롤러
- * 사용자 로그인, 회원가입, 정보 조회, 비밀번호 변경, 회원 탈퇴 기능을 제공합니다.
  */
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/user")
-public class UserController {
+public class UserViewController {
 	private final UserService userService;
 	
 	/**
@@ -63,20 +55,6 @@ public class UserController {
 	    return "user/changePw";
 	}
 	
-	/**
-     * 사용자의 아이디와 현재 비밀번호, 변경할 비밀번호를 받아, 해당 사용자의 비밀번호를 변경합니다.
-     * 변경 성공 시 메인 페이지로 리다이렉트, 실패 시 메시지와 함께 비밀번호 변경 페이지로 리다이렉트합니다.
-     */
-	@PostMapping("/changePw")
-	public String changePassword(ChangePasswordRequestDto requestDto, RedirectAttributes redirectAttributes) {
-	    try {
-	        userService.changePassword(requestDto);
-	        return "redirect:/index";
-	    } catch (IllegalArgumentException e) {
-	        redirectAttributes.addFlashAttribute("msg", e.getMessage());
-	        return "redirect:/user/changePw";
-	    }
-	}
 
 	/**
      * 로그인된 사용자의 정보를 조회하고, 정보를 조회 페이지에 보여줍니다.
@@ -95,7 +73,7 @@ public class UserController {
             model.addAttribute("user_nickname", loggedInUser.getUser_nickname());
             model.addAttribute("user_id", loggedInUser.getUser_id());
             model.addAttribute("user_gender", loggedInUser.getUser_gender());
-//            model.addAttribute("user_grade", loggedInUser.getUser_grade());
+            model.addAttribute("role", loggedInUser.getRole());
         } else {
             // 로그인 안됐을때 로그인 페이지로 이동시키기
         	return "user/login";
@@ -113,29 +91,4 @@ public class UserController {
 	    return "user/resign";
 	}
 	
-	
-	 /**
-     * 사용자의 아이디와 비밀번호를 받아, 해당 사용자를 삭제합니다. 
-     * 삭제 성공 시 메인 페이지로 리다이렉트, 실패 시 회원 탈퇴 페이지로 리다이렉트합니다.
-     * URL: /user/resign
-     */
-	@PostMapping("/resign")
-	public String delete(@RequestParam String user_pw, HttpServletRequest request, RedirectAttributes rttr) {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		
-		org.springframework.security.core.userdetails.User loggedInUser = (org.springframework.security.core.userdetails.User) authentication.getPrincipal();
-		if (loggedInUser == null) {
-			rttr.addFlashAttribute("msg", "로그인이 필요한 서비스입니다.");
-			return "redirect:/user/login";
-		}
-
-		int check = userService.resignUser(loggedInUser.getUsername(), user_pw);
-		if (check == 1) {
-			request.getSession().invalidate();
-			return "redirect:/index";
-		} else {
-			rttr.addFlashAttribute("msg", "비밀번호가 맞지 않습니다.");
-			return "redirect:/user/resign";
-		}
-	}
 }
