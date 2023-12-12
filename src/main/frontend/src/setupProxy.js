5c9a1093c98eb745c436fd18033a24b19dd89032
Yuram-Park <uram422@gmail.com>
2023-12-12 19:41:33 +0900

    후기게시판 React + DB연동

---
+++ src/main/frontend/src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = app => {
+  app.use(
+    createProxyMiddleware(
+      '/dog',
+      {
+		  target: 'http://localhost:8080',
+		  changeOrigin: true
+    })
+  )};
